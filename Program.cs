7109ef2734c0a12618f89695112878996fa0b98b
eth1cspatr10t <85902819+Vilumsy@users.noreply.github.com>
2022-02-10 21:05:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+          new Student() { StudentID = 1, StudentName = "John", Age = 13, } ,
+          new Student() { StudentID = 2, StudentName = "Moin", Age = 21, } ,
+          new Student() { StudentID = 3, StudentName = "Bill", Age = 18, } ,
+          new Student() { StudentID = 4, StudentName = "Ram" , Age = 19, } ,
+          new Student() { StudentID = 5, StudentName = "Ron" , Age = 18, } };
+
+
+
+            Student student = new Student() { StudentID = 1, StudentName = "John", Age = 14 };
+            var test = studentList.Contains(student);
+            Console.WriteLine(test);
+        
+        
+        
+        
+        
+        
+        
+        }
+
+    }
+
+
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp
+{
+    internal class Student
+    {
+
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+        public int StandardID { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            Student sp = (Student)obj;
+
+            if ((StudentID == sp.StudentID) && (StudentName == sp.StudentName) &&((Age == sp.Age)))
+                return true;
+
+            return false;
+        }
+    }
+}
