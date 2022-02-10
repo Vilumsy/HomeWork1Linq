eae8ec856569fd1085175024b8143d2a9f45a6c1
eth1cspatr10t <85902819+Vilumsy@users.noreply.github.com>
2022-02-10 21:19:17 +0200

    test 4

--- Student.cs
+++ Student.cs
@@ -8,7 +8,7 @@ namespace MyApp
 
         public int StudentID { get; set; }
         public string StudentName { get; set; }
-        public int Age { get; set; }
+        public int Age { get; set; } 
         public int StandardID { get; set; }
 
         public override bool Equals(Object obj)
